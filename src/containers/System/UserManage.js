c7cb31ea5dabb6c956af232b2f3f59219709e761
Trungdev <trungdev1611@gmail.com>
2022-06-25 12:50:25 +0700

    Render user and delete user from Reactjs

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,24 +1,60 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
+
 import { connect } from 'react-redux';
-class UserManage extends Component {
+import { useEffect } from 'react'
+import { getAllUser, deleteUserbyId } from './../../services/userService'
+import { useState } from 'react';
+function UserManage() {
+    const [allUser, setAllUser] = useState([])
+    useEffect(() => {
+        getAllUser().then(users => setAllUser(users.data))
+    }, [])
 
-    state = {
+    console.log(allUser)
+
+    async function handleDelete(id) {
+
+        await deleteUserbyId(id)   //return axios.delete('url${id}
+        let data = await getAllUser()
+        console.log(data)
+        setAllUser(data.data)
 
     }
+    return (
+        <>
+            <h2 className="text-center">Manage User</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>STT</th>
+                        <th>Email</th>
+                        <th>FirstName</th>
+                        <th>LastName</th>
+                        <th>Adress</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allUser.length > 0 ? allUser.map((ele, index) => {
+                        return <tr key={index}>
+                            <td>{index}</td>
+                            <td>{ele.email}</td>
+                            <td>{ele.firstName}</td>
+                            <td>{ele.lastName}</td>
+                            <td>{ele.address}</td>
+                            <td>
+                                <button >EditUser</button>
+                                <button onClick={() => handleDelete(ele.id)}>DeleteUser</button>
+                            </td>
+                        </tr>
+                    }) : null}
+                </tbody>
+            </table>
+        </>
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+    );
 }
+
+
 
 const mapStateToProps = state => {
     return {
--- src/services/userService.js
+++ src/services/userService.js
@@ -4,3 +4,9 @@ export const handleLoginservice = (email, password) => {
     return axios.post('/api/login', { email, password })
 }
 
+export const getAllUser = () => {
+    return axios.get('/showuser')
+}
+export const deleteUserbyId = (id) => {
+    return axios.delete(`/deleteuser/${id}`)
+}
